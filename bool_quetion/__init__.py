8a3e11e1b6b771ca3b39fd5fe1d8813c8f592c4b
Pablo Alejandro <pablo@pabli.net.ar>
2025-04-26 01:39:56 -0300

    0.1.7

--- bool_quetion/__init__.py
+++ bool_quetion/__init__.py
@@ -1,14 +1,19 @@
 try:
+    import sys
+    import termios
     from sys import stdin
     from tty import setraw
-    import termios
-    from localized_errors import get_error
     _has_termios = True
 except ImportError:
     raise IndentationError('This function is available only on '
     'Unix-based systems.')
 
-def display_errors(_question, _options, _mark=False):
+try:
+    from .localized_errors import get_error
+except ImportError:
+    from localized_errors import get_error
+
+def display_errors(_question, _options, _err_msg, _mark=False):
     if not isinstance(_options, (list, tuple)):
         raise AttributeError(get_error("invalid_type_options"))
     if isinstance(_options, (list, tuple)) and len(_options) != 2:
@@ -21,11 +26,15 @@ def display_errors(_question, _options, _mark=False):
     del (first_chars)
     if not isinstance(_question, str):
         raise AttributeError(get_error('invalid_type_question'))
+    if not isinstance(_err_msg, (bool, str)):
+        raise AttributeError(get_error('invalid_type_err_mark'))
     if not isinstance(_mark, bool):
         raise AttributeError(get_error('invalid_type_mark'))
 
-def true_false(question, options, mark=False):
-    display_errors(question, options, mark)
+def true_false(question, options, err_msg='Bad key press', mark=False):
+    if isinstance(err_msg, bool):
+        mark = err_msg
+    display_errors(question, options, err_msg, mark)
     title_key = lambda _k: f'\033[1m{_k[0]}\033[0m{_k[1:]}'
     words_question = (
             [f'{title_key(k)}' for k in options]
@@ -36,6 +45,7 @@ def true_false(question, options, mark=False):
     old_settings = termios.tcgetattr(fd)
     first_letter = [k[0].lower() for k in options]
 
+    c = 0
     try:
         setraw(fd)
         while True:
@@ -43,7 +53,15 @@ def true_false(question, options, mark=False):
             if char in first_letter:
                 return char == first_letter[0]
                 break
+            else:
+                if c > 0:
+                    sys.stdout.write('\033[F')  # sube una línea
+                sys.stdout.write('\033[K')  # borra la línea
+                c += 1
+                sys.stdout.write(err_msg + f' ({c})\n')
+                sys.stdout.write('\r')
+                sys.stdout.flush()
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 if __name__ == '__main__':
-    print (true_false('Do you wish to continue?', ['Yes', 'no'], True))
+    print (true_false('Do you wish to continue?', ['Yes', 'no']))
--- bool_quetion/__main__.py
+++
@@ -1,3 +0,0 @@
-def main():
-    print('Hi, I recommend importing the module like this:',
-          'from bool_quetion import true_false', sep='\n')
